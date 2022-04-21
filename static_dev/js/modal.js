387293d767a7bc831a74599f49e0f69bbe8d8187
Eugene Checkoy <mfalcon@mail.ru>
2022-04-21 15:47:03 +0300

    22

--- static_dev/js/modal.js
+++ static_dev/js/modal.js
@@ -1,94 +1,70 @@
-(function () {
-    if (typeof window.CustomEvent === "function") return false;
-    function CustomEvent(event, params) {
-        params = params || { bubbles: false, cancelable: false, detail: null };
-        var evt = document.createEvent('CustomEvent');
-        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-        return evt;
-    }
-    window.CustomEvent = CustomEvent;
-})();
-
-$modal = function (options) {
-    var
-        _elemModal,
-        _eventShowModal,
-        _eventHideModal,
-        _hiding = false,
-        _destroyed = false,
-        _animationSpeed = 200;
-
-    function _createModal(options) {
-        var
-            elemModal = document.createElement('div'),
-            modalTemplate = '<div class="modal__backdrop" data-dismiss="modal"><div class="modal__content"><div class="modal__header"><div class="modal__title" data-modal="title">{{title}}</div><span class="modal__btn-close" data-dismiss="modal" title="Закрыть">×</span></div><div class="modal__body" data-modal="content">{{content}}</div>{{footer}}</div></div>',
-            modalFooterTemplate = '<div class="modal__footer">{{buttons}}</div>',
-            modalButtonTemplate = '<button type="button" class="{{button_class}}" data-handler={{button_handler}}>{{button_text}}</button>',
-            modalHTML,
-            modalFooterHTML = '';
-
-        elemModal.classList.add('modal');
-        modalHTML = modalTemplate.replace('{{title}}', options.title || 'Новое окно');
-        modalHTML = modalHTML.replace('{{content}}', options.content || '');
-        if (options.footerButtons) {
-            for (var i = 0, length = options.footerButtons.length; i < length; i++) {
-                var modalFooterButton = modalButtonTemplate.replace('{{button_class}}', options.footerButtons[i].class);
-                modalFooterButton = modalFooterButton.replace('{{button_handler}}', options.footerButtons[i].handler);
-                modalFooterButton = modalFooterButton.replace('{{button_text}}', options.footerButtons[i].text);
-                modalFooterHTML += modalFooterButton;
-            }
-            modalFooterHTML = modalFooterTemplate.replace('{{buttons}}', modalFooterHTML);
-        }
-        modalHTML = modalHTML.replace('{{footer}}', modalFooterHTML);
-        elemModal.innerHTML = modalHTML;
-        document.body.appendChild(elemModal);
-        return elemModal;
-    }
-
-    function _showModal() {
-        if (!_destroyed && !_hiding) {
-            _elemModal.classList.add('modal__show');
-            document.dispatchEvent(_eventShowModal);
-        }
-    }
-
-    function _hideModal() {
-        _hiding = true;
-        _elemModal.classList.remove('modal__show');
-        _elemModal.classList.add('modal__hiding');
-        setTimeout(function () {
-            _elemModal.classList.remove('modal__hiding');
-            _hiding = false;
-        }, _animationSpeed);
-        document.dispatchEvent(_eventHideModal);
-    }
-
-    function _handlerCloseModal(e) {
-        if (e.target.dataset.dismiss === 'modal') {
-            _hideModal();
-        }
-    }
-
-    _elemModal = _createModal(options || {});
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
 
 
-    _elemModal.addEventListener('click', _handlerCloseModal);
-    _eventShowModal = new CustomEvent('show.modal', { detail: _elemModal });
-    _eventHideModal = new CustomEvent('hide.modal', { detail: _elemModal });
+document.addEventListener('DOMContentLoaded', function() {
 
-    return {
-        show: _showModal,
-        hide: _hideModal,
-        destroy: function () {
-            _elemModal.parentElement.removeChild(_elemModal),
-                _elemModal.removeEventListener('click', _handlerCloseModal),
-                _destroyed = true;
-        },
-        setContent: function (html) {
-            _elemModal.querySelector('[data-modal="content"]').innerHTML = html;
-        },
-        setTitle: function (text) {
-            _elemModal.querySelector('[data-modal="title"]').innerHTML = text;
-        }
-    }
-};
+   /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+   var modalButtons = document.querySelectorAll('.js-open-modal'),
+       overlay      = document.querySelector('.js-overlay-modal'),
+       closeButtons = document.querySelectorAll('.js-modal-close');
+
+
+   /* Перебираем массив кнопок */
+   modalButtons.forEach(function(item){
+
+      /* Назначаем каждой кнопке обработчик клика */
+      item.addEventListener('click', function(e) {
+
+         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+         e.preventDefault();
+
+         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+
+         /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+         modalElem.classList.add('active');
+         overlay.classList.add('active');
+      }); // end click
+
+   }); // end foreach
+
+
+   closeButtons.forEach(function(item){
+
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.modal');
+
+         parentModal.classList.remove('active');
+         overlay.classList.remove('active');
+      });
+
+   }); // end foreach
+
+
+    document.body.addEventListener('keyup', function (e) {
+        var key = e.keyCode;
+
+        if (key == 27) {
+
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        };
+    }, false);
+
+
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+
+
+
+
+}); // end ready
