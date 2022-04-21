2c67664e4fa90a16229f7823a896ce02a350d378
Eugene Checkoy <mfalcon@mail.ru>
2022-04-21 10:12:12 +0300

    почта к формам

--- web/views.py
+++ web/views.py
@@ -114,6 +114,7 @@ def contact(request):
 
         if fback.is_valid():
             Feedback = fback.save(commit=False)
+            cd = form.cleaned_data
             Feedback.save()
             subject = 'Сообщение от {} ({})'.format(cd['name'], cd['email'])
             message = '"{}". {} | {} | {}'.format(cd['text'], cd['name'], cd['phone'], cd['site'])
