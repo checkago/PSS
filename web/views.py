f04d2f4f556f9f07ab37b9934a54466fe4780fc0
checkago <mfalcon@mail.ru>
2022-04-22 20:39:13 +0300

    recaptcha3

--- web/views.py
+++ web/views.py
@@ -138,24 +138,25 @@ def contact(request):
 
             return redirect('/')
 
-            secret_key = settings.RECAPTCHA_SECRET_KEY
-
-            # captcha verification
-            data = {
-                'response': data.get('g-recaptcha-response'),
-                'secret': secret_key
-            }
-            resp = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-            result_json = resp.json()
-
-            print(result_json)
-
-            if not result_json.get('success'):
-                return render(request, 'contact.html', {'is_robot': True})
-
     else:
         fback = FeedbackForm()
 
     return render(request, 'contact.html', {'title': title, 'description': description, 'fback': fback,
                                             'site_key': settings.RECAPTCHA_SITE_KEY})
+
+    secret_key = settings.RECAPTCHA_SECRET_KEY
+
+    # captcha verification
+    data = {
+        'response': data.get('g-recaptcha-response'),
+        'secret': secret_key
+    }
+    resp = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+    result_json = resp.json()
+
+    print(result_json)
+
+    if not result_json.get('success'):
+        return render(request, 'contact.html', {'is_robot': True})
+    # end captcha verification
 
