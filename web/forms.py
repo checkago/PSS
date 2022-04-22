c2737c66acf121a33a06d537b195dd497b0896de
Eugene Checkoy <mfalcon@mail.ru>
2022-04-22 10:43:11 +0300

    21

--- web/forms.py
+++ web/forms.py
@@ -8,6 +8,13 @@ class FeedbackForm(forms.ModelForm):
         widget=forms.Textarea(attrs={'rows': '2'})
     )
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net', 'org', 'xyz', 'de', 'fr', 'ua', 'nl', 'cz', 'group', 'biz']:
+            raise forms.ValidationError(f'Использование почтового ящика в домене .{domain} заблокированно')
+        return email
+
     class Meta:
         model = Feedback
         fields = ('name', 'phone', 'email', 'text', 'site')
