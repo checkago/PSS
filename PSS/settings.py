6d3e5097702825030a06ef77c5bb48c0837b4f5e
Eugene Checkoy <mfalcon@mail.ru>
2022-07-13 11:09:39 +0300

    reCaptcha3

--- PSS/settings.py
+++ PSS/settings.py
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 100000
+DATA_UPLOAD_MAX_MEMORY_SIZE = 1000000
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
